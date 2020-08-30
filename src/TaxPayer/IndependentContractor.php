e359ecd01571946c6a2641e44ce79291ad4ba5db
Victor Lava <victor.lava@aboutyou.com>
2020-08-30 17:02:17 +0300

    Added salary, income calculations in the IndependentContractor class

--- src/TaxPayer/IndependentContractor.php
+++ src/TaxPayer/IndependentContractor.php
@@ -5,21 +5,40 @@ namespace VictorLava\SalaryCalculator\TaxPayer;
 use VictorLava\SalaryCalculator\TaxFormatter;
 
 class IndependentContractor extends AbstractTaxPayer {
-    
+
     public function calculateNet(float $grossSalary)
     {
         $this->setSalaryGross($grossSalary);
 
-        $incomeTax = $grossSalary * $this->taxRates['income_tax'] / 100;
+        $incomeTax = $this->calculateIncomeTax($grossSalary);
+        $netSalary = $this->calculateNetSalary($grossSalary, $incomeTax);
 
-        $net = $grossSalary - $incomeTax;
+        $loss = $this->calculateSalaryLoss($grossSalary, $netSalary);
+        $lossPercentage = $this->calculateSalaryLossInPercetange($loss, $grossSalary);
 
-        $loss = $grossSalary - $net;
-        $lossPercentage = $loss * 100/$grossSalary;
-
-        $this->setSalaryNet($net);
+        $this->setSalaryNet($netSalary);
         $this->setSalaryLoss($loss);
         $this->setSalaryLossPercentage($lossPercentage);
+    }
+
+    public function calculateSalaryLossInPercetange(float $salaryLost, float $grossSalary)
+    {
+        return $salaryLost * 100/$grossSalary;
+    }
+
+    public function calculateSalaryLoss(float $grossSalary, float $netSalary)
+    {
+        return $grossSalary - $netSalary;
+    }
+
+    public function calculateNetSalary(float $grossSalary, float $taxes)
+    {
+        return $grossSalary - $taxes;
+    }
+
+    public function calculateIncomeTax(float $grossSalary)
+    {
+        return $grossSalary * $this->taxRates['income_tax'] / 100;
     }
 
 }
