4282178a6b0c8252b164cb4db89c9f0c5c5a1b3e
Victor Lava <victor.lava@aboutyou.com>
2020-08-30 11:33:08 +0300

    Added Configuration Service Provider and Test

--- src/Configuration.php
+++ src/Configuration.php
@@ -1,1 +1,32 @@
 <?php
+
+namespace VictorLava\SalaryCalculator;
+
+use Noodlehaus\Config;
+use Noodlehaus\Parser\Json;
+
+class Configuration {
+
+    const DIRECTORY = 'config';
+
+    protected $configuration;
+
+    public function load()
+    {
+        $mainConfig = $this->loadMainConfiguration();
+        $mainConfig['taxes'] = $this->loadCountrySpecificConfiguration($mainConfig['default_country']);
+
+        return $mainConfig;
+    }
+
+    public function loadMainConfiguration()
+    {
+        return Config::load(self::DIRECTORY . '/config.json', new Json())->all();
+    }
+
+    public function loadCountrySpecificConfiguration(string $countryCode)
+    {
+        return Config::load(self::DIRECTORY . '/countries/' . $countryCode . '.json', new Json())->all();
+    }
+
+}
--- tests/ConfigurationTest.php
+++ tests/ConfigurationTest.php
@@ -1,1 +1,49 @@
 <?php
+
+namespace VictorLava\SalaryCalculator\Tests;
+
+use Noodlehaus\Config;
+use Noodlehaus\Parser\Json;
+use PHPUnit\Framework\TestCase;
+use VictorLava\SalaryCalculator\Configuration;
+
+class ConfigurationTest extends TestCase
+{
+    private $configurationServiceProvider;
+
+    private $configuration;
+
+    public function __construct(string $name = null, array $data = [], $dataName = '')
+    {
+        $this->configurationServiceProvider = new Configuration();
+        $this->configuration = $this->configurationServiceProvider->loadMainConfiguration();
+
+        parent::__construct($name, $data, $dataName);
+    }
+
+    /** @test */
+    public function load_main_configuration()
+    {
+        $this->assertEquals([
+            'use_external_api' => false,
+            'default_country' => 'lt'
+        ], $this->configuration);
+    }
+
+    /** @test */
+    public function load_country_specific_configurations()
+    {
+        $configurations = $this->configurationServiceProvider->loadCountrySpecificConfiguration($this->configuration['default_country']);
+
+        $this->assertEquals(["employee" => [
+            "social_security_tax" => 6.98,
+            "pension_insurance" => 8.72,
+            "sickness_social_security" => 2.09,
+            "maternity_social_security" => 1.71,
+            "health_insurance" => 0,
+            "working_level" => 100,
+            "income_tax" => 20
+        ]], $configurations);
+    }
+
+}
