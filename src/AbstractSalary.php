43b1cba4bb321e810db40a4406a26cb412d44967
Victor Lava <victor.lava@aboutyou.com>
2020-08-30 16:52:28 +0300

    Added AbstractSalary class

--- src/AbstractSalary.php
+++ src/AbstractSalary.php
@@ -2,6 +2,32 @@
 
 namespace VictorLava\SalaryCalculator;
 
+use VictorLava\SalaryCalculator\TaxPayer\AbstractTaxPayer;
+use VictorLava\SalaryCalculator\TaxPayer\IndependentContractor;
+
 class AbstractSalary {
+
+    protected $salary; // gross
+
+    protected $countryCode;
+
+    protected $taxPayer;
+
+    public function __construct(int $salary, AbstractTaxPayer $taxPayer = null, string $countryCode = null)
+    {
+        $this->salary = $salary;
+        $this->countryCode = $countryCode;
+        $this->taxPayer = $taxPayer;
+
+        if($taxPayer === null) {
+            $this->taxPayer = new IndependentContractor();
+        }
+    }
+
+
+   public function calculate()
+   {
+        return $this->taxPayer->calculate($this->salary);
+   }
 
 }
