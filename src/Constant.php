c3a67bd8fcc0da653106fe5da401a98816cbc14c
Victor Lava <victor.lava@aboutyou.com>
2020-09-01 20:56:27 +0300

    Added new Constant class to hold up the system level constants

---
+++ src/Constant.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace VictorLava\SalaryCalculator;
+
+class Constant {
+
+    const CONFIG_FILE_EXTENSION = "json";
+
+    const DIRECTORIES_THAT_REQUIRE_COUNTRY_CODE = [
+        "rates"
+    ];
+
+}
