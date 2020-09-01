c80393026f6a7f2237e48946e3d51fa820b64398
Victor Lava <victor.lava@aboutyou.com>
2020-09-01 21:31:21 +0300

    Added App model for the main config

---
+++ src/Model/Config/App.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace VictorLava\SalaryCalculator\Model\Config;
+
+use VictorLava\SalaryCalculator\Model\DynamicModel;
+
+class App extends DynamicModel {
+
+    public $fileName = "config";
+
+}
